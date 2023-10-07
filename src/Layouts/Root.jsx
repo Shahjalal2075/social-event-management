134e5bd6e4fca283aa9fb6cee88755a450523542
Shahjalal2075 <shahjalal2075@gmail.com>
2023-10-07 23:59:32 +0600

    Footer Add

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Pages/Shared/Header/Header";
+import Footer from "../Pages/Footer/Footer";
 
 const Root = () => {
     return (
         <div className="container mx-auto">
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
