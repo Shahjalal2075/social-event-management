e593ce44b66d75399e9d9f69e07bce7c06436cae
Shahjalal2075 <shahjalal2075@gmail.com>
2023-10-09 23:01:25 +0600

    AOS Add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Banner from "../Banner/Banner";
 import Portfolio from "../Portfolio/Portfolio";
 import AnniversariesHome from "./AnniversariesHome";
@@ -6,19 +7,45 @@ import BirthdayHome from "./BirthdayHome";
 import EngagementHome from "./EngagementHome";
 import RetirementHome from "./RetirementHome";
 import WeddingsHome from "./WeddingsHome";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+
+    useEffect(() => {
+        AOS.init({
+          duration: 3000,
+          once: true,
+        });
+      }, []);
+
     return (
         <div>
             <div className="mt-5">
-                <Banner></Banner>
-                <WeddingsHome></WeddingsHome>
-                <BirthdayHome></BirthdayHome>
-                <AnniversariesHome></AnniversariesHome>
-                <EngagementHome></EngagementHome>
-                <RetirementHome></RetirementHome>
-                <BabyShowerHome></BabyShowerHome>
-                <Portfolio></Portfolio>
+                <div data-aos="fade-down" className="">
+                    <Banner></Banner>
+                </div>
+                <div data-aos="fade-right" className="">
+                    <WeddingsHome></WeddingsHome>
+                </div>
+                <div data-aos="fade-left" className="">
+                    <BirthdayHome></BirthdayHome>
+                </div>
+                <div data-aos="fade-right" className="">
+                    <AnniversariesHome></AnniversariesHome>
+                </div>
+                <div data-aos="fade-left" className="">
+                    <EngagementHome></EngagementHome>
+                </div>
+                <div data-aos="fade-right" className="">
+                    <RetirementHome></RetirementHome>
+                </div>
+                <div data-aos="fade-left" className="">
+                    <BabyShowerHome></BabyShowerHome>
+                </div>
+                <div data-aos="fade-up" className="">
+                    <Portfolio></Portfolio>
+                </div>
             </div>
         </div>
     );
