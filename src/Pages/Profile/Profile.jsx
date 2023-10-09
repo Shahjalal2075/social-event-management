96686a69f48bffc849b74b231a690d5a1c0a7212
Shahjalal2075 <shahjalal2075@gmail.com>
2023-10-09 23:26:04 +0600

    Profile section edit

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -16,7 +16,10 @@ const Profile = () => {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <img className="mask mask-circle w-52" src={user.photoURL} />
+            {
+                user.photoURL ? <img className="mask mask-circle w-52" src={user.photoURL} /> : <img className="mask mask-circle w-52" src="https://i.ibb.co/0rcvLrD/users.png" />
+            }
+            
             <h2 className="font-bold text-2xl my-6">Name: {user.displayName}</h2>
             <h2 className="font-bold text-2xl mb-6">Email: {user.email}</h2>
             <button onClick={handleLogOut} className="btn font-bold btn-neutral text-[#fff]">Logout</button>
